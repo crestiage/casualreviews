bd3aeafcf3aa69b1ebbbf160c364034f508252c9
Timothy Roasa <timothy.roasa@haelthtech.com>
2020-05-14 02:16:10 +0800

    Main Page

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -16,6 +16,8 @@
         <script src="resources/js/jquery-3.4.1.min.js"></script>
         <script src="resources/js/bootstrap.min.js"></script>
 
+        <script src="resources/js/font-awesome.js" crossorigin="anonymous"></script>
+
     </head>
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -49,11 +51,60 @@
                 <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
             </li>
             </ul>
-            <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
         </div>
+        <span class="navbar-text">
+            <a href="#">Log in</a> / <a href="#">Sign up</a>
+        </span>
     </nav>
 
+    <body>
+        <div class="container-lg">    
+            <div class="row" style="margin-top: 20px;">
+                <div class="col-3">
+                    <div class="row margin-bot-10">
+                        <div class="col">
+                            <form class="form-inline my-2 my-lg-0">
+                                <input class="form-control mr-sm-2" type="search" placeholder="Search Tags" aria-label="Search">
+                                <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+                            </form>
+                        </div>
+                    </div>
+
+                    <div class="row margin-bot-10">
+                        <div class="col">
+                            <h5>Popular Tags</h5>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Technology</li>
+                                <li class="list-group-item">Gaming</li>
+                                <li class="list-group-item">Food</li>
+                                <li class="list-group-item">Kingston</li>
+                                <li class="list-group-item">Grammar</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-7">
+                    <!-- Content -->
+                    @yield("content")
+                </div>
+                <div class="col-2">
+                    <div class="row margin-bot-10">
+                        <div class="col bg-casual-reviews" style="height: 700px;">
+                            ADS
+                        </div>
+                    </div>
+                    <div class="row margin-bot-10">
+                        <div class="col">
+                            <a href="#">
+                                <img src="resources/img/site/paypal-donate-button.png" class="img-fluid">
+                            </a>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+
+    </body>
 </html>
---
+++ resources/views/main.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.template')
+
+@section("content")
+<div class="row review-row">
+    <div class="col">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Review Title 1</h5>
+                <img src="resources/img/site/placeholder.jpg" class="img-fluid margin-bot-10" width="100%"/>
+                <h6 class="card-subtitle mb-2 text-muted">
+                    Tags: <a href="#">Technology</a>, <a href="#">Gaming</a>
+                </h6>
+                <p class="card-text" style="margin-top: 10px;">Some quick example text to build on the card title and make up the bulk of the card's content. <a href="#" class="card-link">Read More</a></p>
+                <a href="https://www.facebook.com/sharer/sharer.php?u=example.org" target="_blank" class="card-link btn fb-share-button">
+                    <i class="fab fa-facebook-f" style="margin-right: 5px;"></i> Share on Facebook
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row review-row">
+    <div class="col">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Review Title 2</h5>
+                <img src="resources/img/site/placeholder.jpg" class="img-fluid margin-bot-10" width="100%"/>
+                <h6 class="card-subtitle mb-2 text-muted">
+                    Tags: <a href="#">Technology</a>, <a href="#">Gaming</a>
+                </h6>
+                <p class="card-text" style="margin-top: 10px;">Some quick example text to build on the card title and make up the bulk of the card's content. <a href="#" class="card-link">Read More</a></p>
+                <a href="https://www.facebook.com/sharer/sharer.php?u=example.org" target="_blank" class="card-link btn fb-share-button">
+                    <i class="fab fa-facebook-f" style="margin-right: 5px;"></i> Share on Facebook
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row review-row">
+    <div class="col">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Review Title 3</h5>
+                <img src="resources/img/site/placeholder.jpg" class="img-fluid margin-bot-10" width="100%"/>
+                <h6 class="card-subtitle mb-2 text-muted">
+                    Tags: <a href="#">Technology</a>, <a href="#">Gaming</a>
+                </h6>
+                <p class="card-text" style="margin-top: 10px;">Some quick example text to build on the card title and make up the bulk of the card's content. <a href="#" class="card-link">Read More</a></p>
+                <a href="https://www.facebook.com/sharer/sharer.php?u=example.org" target="_blank" class="card-link btn fb-share-button">
+                    <i class="fab fa-facebook-f" style="margin-right: 5px;"></i> Share on Facebook
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
